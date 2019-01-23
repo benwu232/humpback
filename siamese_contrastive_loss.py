correct bugs in transform and normalization

siamese_contrastive_loss.py
@@ -6,7 +6,7 @@ import torchvision
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 #some parameters
-debug = 1
+debug = 0
 enable_lr_find = 1
 now_str = now2str(format="%Y-%m-%d_%H-%M-%S")
 txlog = tx.SummaryWriter(f'../tblog/{now_str}')
@@ -61,31 +61,6 @@ file_lut = df0.set_index('Image').to_dict()
 im_tfms = get_transforms(do_flip=False, max_zoom=1, max_warp=0, max_rotate=2)
 
 
-data = (
-    ImageItemList
-        # .from_df(df_known, 'data/train', cols=['Image'])
-        .from_folder(train_path)
-        # .split_by_idxs(train_item_list, val_item_list)
-        .split_by_valid_func(lambda path: path2fn(str(path)) in val_list)
-        # .split_by_idx(val_list)
-        # .random_split_by_pct(seed=SEED)
-        .label_from_func(lambda path: fn2label[path2fn(str(path))])
-        #.add_test(ImageItemList.from_folder(test_path))
-        .transform([None, None], size=im_size, resize_method=ResizeMethod.SQUISH)
-        #.transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
-        .databunch(bs=train_batch_size, num_workers=dl_workers, path=root_path)
-        #.normalize(imagenet_stats)
-)
-
-data.add_tfm(normalize_batch)
-
-train_dl = DataLoader(
-    SiameseDs(data.train_ds),
-    batch_size=train_batch_size,
-    shuffle=True,
-    #collate_fn=siamese_collate,
-    num_workers=dl_workers
-)
 
 data_v = (
     ImageItemList
@@ -98,9 +73,9 @@ data_v = (
         .label_from_func(lambda path: fn2label[path2fn(str(path))])
         .add_test(ImageItemList.from_folder(test_path))
         #.transform([None, None], size=im_size, resize_method=ResizeMethod.SQUISH)
-        #.transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
+        .transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
         .databunch(bs=train_batch_size, num_workers=dl_workers, path=root_path)
-        .normalize(imagenet_stats)
+        #.normalize(imagenet_stats)
 )
 
 #v = SimpleDataset(data.valid)
@@ -128,7 +103,39 @@ ref_dl = DataLoader(
     num_workers=dl_workers
 )
 
-data_bunch = ImageDataBunch(train_dl, valid_dl, fix_dl=ref_dl)
+data = (
+    ImageItemList
+        # .from_df(df_known, 'data/train', cols=['Image'])
+        .from_folder(train_path)
+        # .split_by_idxs(train_item_list, val_item_list)
+        .split_by_valid_func(lambda path: path2fn(str(path)) in val_list)
+        # .split_by_idx(val_list)
+        # .random_split_by_pct(seed=SEED)
+        .label_from_func(lambda path: fn2label[path2fn(str(path))])
+        #.add_test(ImageItemList.from_folder(test_path))
+        #.transform([None, None], size=im_size, resize_method=ResizeMethod.SQUISH)
+        .transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
+        .databunch(bs=train_batch_size, num_workers=dl_workers, path=root_path)
+        #.normalize(imagenet_stats)
+)
+
+#data.add_tfm(normalize_batch)
+
+train_dl = DataLoader(
+    SiameseDs(data.train_ds, data.train_dl),
+    batch_size=train_batch_size,
+    shuffle=True,
+    #collate_fn=siamese_collate,
+    num_workers=dl_workers
+)
+
+tmp_data_bunch = ImageDataBunch(train_dl, data_v.valid_dl)
+tmp_data_bunch.add_tfm(normalize_batch)
+
+data_bunch = ImageDataBunch(train_dl, valid_dl, fix_dl=ref_dl, test_dl=test_dl)
+data_bunch.normalize(imagenet_stats)
+
+data_bunch.train_dl = tmp_data_bunch.train_dl
 
 '''
 data_bunch.train_dl = DataLoaderTrain1(train_dl, device, tfms=im_tfms[0], collate_fn=collate_siamese)
@@ -149,11 +156,11 @@ data_bunch.fix_dl = DataLoaderVal(ref_dl, device, tfms=None, collate_fn=data_col
 #
 #exit()
 
-#siamese = SiameseNet(emb_len, arch=arch, width=im_size, height=im_size, diff_method=diff_method)
-siamese = SiameseNetwork2(arch=arch)
+siamese = SiameseNet(emb_len, arch=arch, width=im_size, height=im_size, diff_method=diff_method, drop_rate=0.0)
+#siamese = SiameseNetwork2(arch=arch)
 
 # new_whale should not be involved in positive distance
-new_whale_idx = find_new_whale_idx(data.train_ds.y.classes)
+#new_whale_idx = find_new_whale_idx(data.train_ds.y.classes)
 #triploss = TripletLoss(margin=0.2)
 contrastive_loss = ContrastiveLoss(margin=2.0)
 
@@ -169,9 +176,8 @@ cb_save_model = SaveModelCallback(learn, every="epoch", name=f"siamese")
 cb_siamese_validate = SiameseValidateCallback(learn, txlog)
 cbs = [cb_save_model, cb_siamese_validate]
 
+learn.split([learn.model.cnn[:6], learn.model.cnn[6:], learn.model.fc1])
 learn.freeze_to(-1)
-learn.fit_one_cycle(1)
-learn.unfreeze()
 
 if enable_lr_find:
     print('LR plotting ...')
@@ -179,8 +185,10 @@ if enable_lr_find:
     learn.recorder.plot()
     plt.savefig('lr_find.png')
 
-max_lr = 3e-5
-#lrs = [max_lr/100, max_lr/10, max_lr]
-#learn.fit_one_cycle(300, lrs)
-learn.fit_one_cycle(300, max_lr, callbacks=cbs)
+learn.fit_one_cycle(3, 1e-4)
+learn.unfreeze()
+
+max_lr = 1e-4
+lrs = [max_lr/100, max_lr/10, max_lr]
+learn.fit_one_cycle(300, lrs, callbacks=cbs)
 #learn.fit_one_cycle(300, max_lr)

siamese_triplet_loss.py
@@ -6,7 +6,7 @@ import torchvision
 torch.multiprocessing.set_sharing_strategy('file_system')
 
 #some parameters
-debug = 0
+debug = 1
 enable_lr_find = 1
 now_str = now2str(format="%Y-%m-%d_%H-%M-%S")
 txlog = tx.SummaryWriter(f'../tblog/{now_str}')
@@ -21,7 +21,7 @@ if debug == 1:
 else:
     train_batch_size = 64
     val_batch_size = 256
-    dl_workers = 2
+    dl_workers = 4
 seed = 1
 
 emb_len = 128
@@ -47,7 +47,6 @@ if debug:
     val_list = val_list_dbg
 else:
     train_list, val_list = split_whale_set(df0, nth_fold=0, new_whale_method=1, seed=1)
-print(len(train_list), len(val_list))
 
 im_count = df0[df0.Id != 'new_whale'].Id.value_counts()
 im_count.name = 'sighting_count'
@@ -60,7 +59,34 @@ file_lut = df0.set_index('Image').to_dict()
 
 im_tfms = get_transforms(do_flip=False, max_zoom=1, max_warp=0, max_rotate=2)
 
+
 data = (
+    ImageItemList
+        # .from_df(df_known, 'data/train', cols=['Image'])
+        .from_folder(train_path)
+        # .split_by_idxs(train_item_list, val_item_list)
+        .split_by_valid_func(lambda path: path2fn(str(path)) in val_list)
+        # .split_by_idx(val_list)
+        # .random_split_by_pct(seed=SEED)
+        .label_from_func(lambda path: fn2label[path2fn(str(path))])
+        #.add_test(ImageItemList.from_folder(test_path))
+        .transform([None, None], size=im_size, resize_method=ResizeMethod.SQUISH)
+        #.transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
+        .databunch(bs=train_batch_size, num_workers=dl_workers, path=root_path)
+        .normalize(imagenet_stats)
+)
+
+#data.add_tfm(normalize_batch)
+
+train_dl = DataLoader(
+    SiameseDsTriplet(data.train_ds),
+    batch_size=train_batch_size,
+    shuffle=True,
+    #collate_fn=siamese_collate,
+    num_workers=dl_workers
+)
+
+data_v = (
     ImageItemList
         # .from_df(df_known, 'data/train', cols=['Image'])
         .from_folder(train_path)
@@ -72,21 +98,15 @@ data = (
         .add_test(ImageItemList.from_folder(test_path))
         #.transform([None, None], size=im_size, resize_method=ResizeMethod.SQUISH)
         #.transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
-        #.databunch(bs=BS, num_workers=NUM_WORKERS, path=root_path)
-        #.normalize(imagenet_stats)
+        .databunch(bs=train_batch_size, num_workers=dl_workers, path=root_path)
+        .normalize(imagenet_stats)
 )
 
-train_dl = DataLoader(
-    SiameseDsTriplet(data.train),
-    batch_size=train_batch_size,
-    shuffle=True,
-    #collate_fn=siamese_collate,
-    num_workers=dl_workers
-)
+print(f'train_set: {len(data_v.train_ds)}, val_set: {len(data_v.valid_ds)}, test_set: {len(data_v.test_ds)}')
 
 #v = SimpleDataset(data.valid)
 valid_dl = DataLoader(
-    SimpleDataset(data.valid),
+    SimpleDataset(data_v.valid_ds),
     batch_size=val_batch_size,
     shuffle=False,
     drop_last=False,
@@ -94,57 +114,22 @@ valid_dl = DataLoader(
 )
 
 test_dl = DataLoader(
-    SimpleDataset(data.test),
+    SimpleDataset(data_v.test_ds),
     batch_size=val_batch_size,
     shuffle=False,
     drop_last=False,
     num_workers=dl_workers
 )
 
-data_ref = (
-    ImageItemList
-        .from_df(df_known, train_path, cols=['Image'])
-        #.from_folder(train_path)
-        # .split_by_idxs(train_item_list, val_item_list)
-        .split_by_valid_func(lambda path: path2fn(str(path)) in val_list)
-        # .split_by_idx(val_list)
-        # .random_split_by_pct(seed=SEED)
-        .label_from_func(lambda path: fn2label[path2fn(str(path))])
-        #.add_test(ImageItemList.from_folder(test_path))
-        #.transform([None, None], size=im_size, resize_method=ResizeMethod.SQUISH)
-        #.transform(im_tfms, size=im_size, resize_method=ResizeMethod.SQUISH)
-        #.databunch(bs=BS, num_workers=NUM_WORKERS, path=root_path)
-        #.normalize(imagenet_stats)
+ref_dl = DataLoader(
+    SimpleDataset(data_v.train_ds),
+    batch_size=val_batch_size,
+    shuffle=False,
+    #collate_fn=siamese_collate,
+    num_workers=dl_workers
 )
-print(len(data_ref.train), len(data_ref.valid))
 
-#gen_ref_ds(data.train)
-if debug == 0:
-    ref_dl = DataLoader(
-        SimpleDataset(data_ref.train),
-        batch_size=val_batch_size,
-        shuffle=False,
-        #collate_fn=siamese_collate,
-        num_workers=dl_workers
-    )
-else:
-    ref_dl = DataLoader(
-        SimpleDataset(data.train),
-        batch_size=val_batch_size,
-        shuffle=False,
-        #collate_fn=siamese_collate,
-        num_workers=dl_workers
-    )
-
-
-data_bunch = ImageDataBunch(train_dl, valid_dl, fix_dl=ref_dl, collate_fn=collate_siamese_triplet)
-data_bunch.train_dl = DataLoaderTrain(train_dl, device, tfms=im_tfms[0], collate_fn=collate_siamese_triplet)
-#data_bunch.valid_dl = DataLoaderMod(valid_dl, None, None, siamese_collate)
-data_bunch.valid_dl = DataLoaderVal(valid_dl, device, tfms=None, collate_fn=data_collate)
-data_bunch.test_dl = DataLoaderVal(test_dl, device, tfms=None, collate_fn=data_collate)
-data_bunch.fix_dl = DataLoaderVal(ref_dl, device, tfms=None, collate_fn=data_collate)
-#data_bunch.add_tfm(normalize_batch)
-#data_bunch.valid_dl = None
+data_bunch = ImageDataBunch(train_dl, valid_dl, fix_dl=ref_dl)
 
 #for batch in data_bunch.train_dl:
 #    print(len(batch))
@@ -158,7 +143,7 @@ data_bunch.fix_dl = DataLoaderVal(ref_dl, device, tfms=None, collate_fn=data_col
 siamese = SiameseNet(emb_len, arch=arch, width=im_size, height=im_size, diff_method=diff_method)
 
 # new_whale should not be involved in positive distance
-new_whale_idx = find_new_whale_idx(data.train.y.classes)
+new_whale_idx = find_new_whale_idx(data.train_ds.y.classes)
 triploss = TripletLoss(margin=0.2)
 
 learn = LearnerEx(data_bunch,
@@ -173,7 +158,8 @@ cb_save_model = SaveModelCallback(learn, every="epoch", name=f"siamese")
 cb_siamese_validate = SiameseValidateCallback(learn, txlog)
 cbs = [cb_save_model, cb_siamese_validate]
 
-learn.fit_one_cycle(1, callbacks=cbs)
+learn.freeze_to(-1)
+learn.fit_one_cycle(1)
 learn.unfreeze()
 
 if enable_lr_find:
@@ -186,3 +172,4 @@ max_lr = 3e-5
 #lrs = [max_lr/100, max_lr/10, max_lr]
 #learn.fit_one_cycle(300, lrs)
 learn.fit_one_cycle(300, max_lr, callbacks=cbs)
+#learn.fit_one_cycle(300, max_lr)

utils.py
@@ -331,8 +331,9 @@ def make_class_idx_dict(ds, ignore=[]):
     return class_idx_dict
 
 class SiameseDs(torch.utils.data.Dataset):
-    def __init__(self, ds):
+    def __init__(self, ds, dl):
         self.ds = ds
+        self.dl = dl
         self.idx2class = ds.y.items
         #self.id_dict = make_id_dict(self.whale_class_dict)
         self.class_dict = make_class_idx_dict(ds)
@@ -347,12 +348,17 @@ class SiameseDs(torch.utils.data.Dataset):
             while True:
                 idx_p, pos_valid = self.find_same(idx)
                 if pos_valid:
-                    return (self.ds[idx][0], self.ds[idx_p][0]), 1
+                    im1, label1 = self.dl.__getitem__(idx)
+                    im2, label2 = self.dl.__getitem__(idx_p)
+                    assert label1.obj == label2.obj
+                    return (im1, im2), 1
                 else:
                     idx = np.random.randint(self.len)
         else:
             idx_n = self.find_different(idx)
-            return (self.ds[idx][0], self.ds[idx_n][0]), 0
+            im1, label1 = self.dl.__getitem__(idx)
+            im3, label3 = self.dl.__getitem__(idx_n)
+            return (im1, im3), 0
 
     def find_same(self, idx):
         whale_class = self.idx2class[idx]
@@ -392,7 +398,7 @@ class SiameseDsTriplet(SiameseDs):
         idx_a = idx
         idx_p, pos_valid = self.find_same(idx)
         idx_n = self.find_different(idx)
-        return self.ds[idx_a][0], self.ds[idx_p][0], self.ds[idx_n][0], pos_valid
+        return (self.ds[idx_a][0], self.ds[idx_p][0], self.ds[idx_n][0]), pos_valid
 
 
 def collate_siamese(items):
@@ -439,12 +445,12 @@ def cnn_activations_count(model, width, height):
 
 
 class SiameseNet(nn.Module):
-    def __init__(self, emb_len=128, arch=models.resnet18, width=224, height=224, diff_method=1):
+    def __init__(self, emb_len=128, arch=models.resnet18, width=224, height=224, diff_method=1, drop_rate=0.3):
         super().__init__()
         self.cnn = create_body(arch)
         self.fc1 = nn.Linear(cnn_activations_count(arch, width, height), emb_len)
         self.fc2 = nn.Linear(emb_len, 1)
-        self.dropout = nn.Dropout(0.0)
+        self.dropout = nn.Dropout(drop_rate)
         self.diff_method = diff_method
         if self.diff_method == 1:
             self.diff_vec = self.diff_vec1
@@ -457,8 +463,8 @@ class SiameseNet(nn.Module):
     def im2emb(self, im):
         x = self.cnn(im)
         x = self.flatten(x)
-        x = self.fc1(x)
         x = self.dropout(x)
+        x = self.fc1(x)
         return x
 
     def diff_vec1(self, emb1, emb2):
@@ -475,12 +481,18 @@ class SiameseNet(nn.Module):
         logits = self.fc2(x)
         return torch.sigmoid(logits)
 
-    def forward(self, batch):
+    def forward1(self, batch):
         emb1 = self.im2emb(batch['im1'])
         emb2 = self.im2emb(batch['im2'])
         diff = self.diff_vec1(emb1, emb2)
         similarity = self.similarity(diff)
         return similarity
+
+    def forward(self, im1, im2):
+        x1 = self.im2emb(im1)
+        x2 = self.im2emb(im2)
+        dist = self.distance(x1, x2)
+        return dist
 
 
 from functional import seq
@@ -690,10 +702,13 @@ def stat_distance(dist_matrix, row_labels, col_labels, mask_labels=[]):
     #find pos-max and neg-min
     pos_dists_max0 = (dist_matrix * pos_mask).sort()[0][:, -1]
     pos_dist_max = pos_dists_max0[pos_dists_max0 != 0].mean()
+    del pos_mask
 
     neg_matrix = dist_matrix * neg_mask
+    del dist_matrix
     neg_matrix[neg_matrix == 0] += neg_matrix.max() #to get min
     neg_dist_min = neg_matrix.sort()[0][:, 0].mean()
+    del neg_matrix
 
     return pos_dist_max, neg_dist_min
 
@@ -808,6 +823,7 @@ def siamese_mat(in_dl, model, rf_dl, pos_mask=[], ref_idx2class=[], target_idx2c
         # cal map5
         top5_matrix, map5 = cal_mapk(distance_matrix, val_target_tensor, k=5, ref_idx2class=ref_idx2class, target_idx2class=target_idx2class)
         print(f'map5 = {map5}')
+        torch.cuda.empty_cache()
         return map5, top5_matrix, dist_pos_max, dist_neg_min
 
 class SiameseValidateCallback(fastai.callbacks.tracker.TrackerCallback):