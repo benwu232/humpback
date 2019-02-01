bc557f946d14759bae7758297d725595cf4cbfd7
wb <benwu232@gmail.com>
2019-02-01 16:50:32 +0800

    add old whale data tofluke_detection_ex, now it has 1600 images

--- fluke_detection.py
+++ fluke_detection.py
@@ -9,17 +9,45 @@ import pandas as pd
 from torch import optim
 import re
 import json
+import random
 #import cv2
 import types
 
 from utils import *
 
-root_dir = Path('../input')
+root_dir = Path('./../input')
 train_dir = root_dir/'train'
 annot_dir = root_dir/'annotation'
+old_train_dir = root_dir/'old/train'
+
+def points2bbox(points):
+    left, top = points[0]
+    right, bottom = points[0]
+    for x, y in points[1:]:
+        left = min(left, x)
+        top = min(top, y)
+        right = max(right, x)
+        bottom = max(bottom, y)
+    return [top, left, bottom, right]
 
 
-j = json.load(open(f'{annot_dir}/annotations.json'))
+with open(annot_dir/'cropping.txt', 'rt') as f:
+    data = f.read().split('\n')[:-1]
+data = [line.split(',') for line in data]
+old_annot = [(p,[(int(coord[i]),int(coord[i+1])) for i in range(0,len(coord),2)]) for p,*coord in data]
+
+root_dir/'old/train'/old_annot[0][0]
+
+points2bbox(old_annot[0][1])
+
+def relative_path(original_path, relative_to=root_dir):
+    return pathlib.PurePath(original_path).relative_to(relative_to)
+
+old_fn2bbox = {relative_path(root_dir/'old/train'/anno[0]): [[points2bbox(anno[1])], ['fluke']] for anno in old_annot}
+
+
+
+new_annot = json.load(open(f'{annot_dir}/annotations.json'))
 
 SZ = 224
 BS = 64
@@ -40,33 +68,45 @@ def anno2bbox(anno):
         np.clip(anno['x']+anno['width'], 0, im_width) / im_width * SZ
     ]
 
-fn2bbox = {jj['filename']: [[anno2bbox(jj)], ['fluke']] for jj in j}
-path2fn = lambda path: re.search('\w*\.jpg$', path).group(0)
-get_y_func = lambda o: fn2bbox[path2fn(o)]
+fn2bbox = {relative_path(root_dir/'train'/jj['filename']): [[anno2bbox(jj)], ['fluke']] for jj in new_annot}
+fn2bbox.update(old_fn2bbox)
+
+get_y_func = lambda o: fn2bbox[relative_path(o)]
 
 #def get_y_func(o):
-#    return fn2bbox[path2fn(o)]
+#    return fn2bbox[relative_path(o)]
 
-idxs = np.arange(len(j))
+
+n_val = 300
+fn_list = list(fn2bbox.keys())
+random.seed(0)
+random.shuffle(fn_list)
+val_fns = fn_list[:n_val]
+trn_fns = fn_list[n_val:]
+
+'''
+idxes = np.arange(len(fn2bbox))
 np.random.seed(0)
-np.random.shuffle(idxs)
-val_idxs = idxs[:100]
+np.random.shuffle(idxes)
+val_idxes = idxes[:300]
 
-val_j = [anno for i, anno in enumerate(j) if i in val_idxs]
-trn_j = [anno for i, anno in enumerate(j) if i not in val_idxs]
+val_j = [anno for i, anno in enumerate(j) if i in val_idxes]
+trn_j = [anno for i, anno in enumerate(j) if i not in val_idxes]
 len(trn_j), len(val_j)
 
-pd.to_pickle([anno['filename'] for anno in val_j], f'{annot_dir}/val_fns_detection.pkl') # this will allow me to use the same validation set across NBs
-val_fns = pd.read_pickle(f'{annot_dir}/val_fns_detection.pkl')
-
+#pd.to_pickle([anno['filename'] for anno in val_j], f'{annot_dir}/val_fns_detection.pkl') # this will allow me to use the same validation set across NBs
+#val_fns = pd.read_pickle(f'{annot_dir}/val_fns_detection.pkl')
+val_fns = [anno['filename'] for anno in val_j]
 val_fns[0]
+'''
 
 class StubbedObjectCategoryList(ObjectCategoryList):
     def analyze_pred(self, pred):
         return [pred.unsqueeze(0), torch.ones(1).long()]
 
-data = (ObjectItemList.from_df(pd.DataFrame(data=list(fn2bbox.keys())), path=train_dir)
-        .split_by_valid_func(lambda path: path2fn(path) in val_fns)
+data = (ObjectItemList.from_df(pd.DataFrame(data=fn_list), path=root_dir)
+        .split_by_valid_func(lambda path: relative_path(path) in val_fns)
+        #.split_by_valid_func(val_func)
         .label_from_func(get_y_func, label_cls=StubbedObjectCategoryList)
         .transform(get_transforms(max_zoom=1, max_warp=0.05, max_rotate=0.05, max_lighting=0.2), tfm_y=True, size=(SZ,SZ), resize_method=ResizeMethod.SQUISH)
         .databunch(bs=BS, num_workers=NUM_WORKERS)
